14bc382cb2a0e14a279efa392662dfe47d99a00d
DarklerBrian <darklerbrian1025@gmail.com>
2022-07-01 09:10:23 -0700

    Updates

---
+++ precache-manifest.472ece0932b780307f82812c318375f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de33be7ae5bf0696fc5",
+    "url": "/static/css/main.bdee8a49.chunk.css"
+  },
+  {
+    "revision": "7de33be7ae5bf0696fc5",
+    "url": "/static/js/main.7de33be7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17064abad5bc83d05565afb4fd1aa6be",
+    "url": "/index.html"
+  }
+];
